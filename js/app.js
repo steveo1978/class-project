e22c3996f7183e0945894ac2a346d2c8299c5dfe
steveo1978 <steve.r.marical@outlook.com>
2020-07-13 22:42:57 -0700

    updating js

---
+++ js/app.js
@@ -0,0 +1,37 @@
+function addGreeting() {
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
+
+    if (hourNow > 18) {
+        greeting = 'Good evening ! ';
+    } else if (hourNow > 12) {
+        greeting = 'Good afternoon ! ';
+    } else if (hourNow > 0) {
+        greeting = 'Good morning ! ';
+    } else {
+        greeting = 'Welcome ! ';
+    }
+    return document.write('<h3>' + greeting + '</h3>');
+}
+function addUsername() {
+    var name = prompt('What is your name ?');
+    var message = 'Hello,  ' + name;
+    }
+    return document.write('<h3>' + message + '</h3>');
+
+function askPrefence() {
+    var toiletQty = prompt('Do you have one or two toilets?');
+    var response;
+
+    if (toiletQty === 'one') {
+        response = 'Would you like to learn about The Crapper?';
+    } else if (toiletQty === 'two') {
+        response = 'Two is always better than one';
+    } else {
+        response = 'YOU CHOSE POORLY !'; 
+    }
+    return document.write('<h3>' + response + '</h3>');
+}
+        
+    
--- js/ask-name.js
+++
@@ -1,4 +0,0 @@
-var name = prompt('What is your name ?');
-var message = 'Hello,  ' + name;
-
-document.write('<h3>' + message + '</h3>');
